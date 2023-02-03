14177293543f2d9ad71f179b300442c21a4b3b3a
Aleks K <alx.kan037@gmail.com>
2023-02-03 13:23:15 +0500

    Add alert

--- Lection15_Alerts/Lection15_Alerts/ViewController.swift
+++ Lection15_Alerts/Lection15_Alerts/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you alow tu use photos?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
     }
     
     
